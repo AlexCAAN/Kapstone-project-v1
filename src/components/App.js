95f73486a6a99888a05a5470327ed68ac7b46a62
Alexcaan <Alexcaan1020@gmail.com>
2021-12-20 10:07:51 -0700

    initial-commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+
+
+import '../styles/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>your mum</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
